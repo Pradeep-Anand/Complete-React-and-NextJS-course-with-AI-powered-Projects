8295fdc251cc1c1ca71d27c3c8a4516f95034d6b
Pradeep Anand <panandinnitp@gmail.com>
2026-01-22 14:58:15 +0530

    Build Final Counter App

--- 1. Counter/src/App.jsx
+++ 1. Counter/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Button from "./Button.jsx"
 function App() {
+<<<<<<< HEAD
   const [count, setCount] = useState(0)
   const [countSet, setCountSet] = useState(0)
   return (
@@ -28,6 +29,33 @@ function App() {
       </div>
     </div>
 
+=======
+  const [state, setState] = useState(0)
+  function funIncrease(state){
+    state = state+1;
+    return state;
+  }
+  return (
+    <>
+    <h1 className="flex justify-center m-8 text-4xl text-emerald-500 underline">Counter App</h1>
+    <div className="flex justify-center rounded">
+      <p className="flex justify-center m-1 p-1 text-2xl border-1 inline bg-blue-100 text-cyan-500">Count is {state}</p>
+    </div>
+    
+
+    <div className="flex justify-center">
+      <Button name="Increase" fun="funIncrease()"></Button>
+      <Button name="Decrease"></Button>
+    </div>
+    <div className="mx-112">
+      <label htmlFor="setToInput" className="m-2 p-2">Set Counter Value:</label>
+      <input type="number" placeholder="Type any Number to set Counter." id="setToInput" className="border-2 rounded p-1 color-greenc"/>
+      <Button name="Set to"></Button>
+    </div>
+      
+
+    
+>>>>>>> 13c1d77 (tart Building Counter Project - Frontend Completed)
     </>
   )
 }
